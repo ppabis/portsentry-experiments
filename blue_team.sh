b1760a51f244b8d32ba8f3dcbb4ff2ec4308a511
ppabis <piotr@pabis.eu>
2025-06-22 19:43:53 +0200

    Install portsentry and docker for blue team

---
+++ blue_team.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+hostnamectl set-hostname blue-team
+
+# Install portsentry
+apt update
+apt install portsentry -y
+
+# Install Docker
+apt install ca-certificates curl gnupg lsb-release -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
