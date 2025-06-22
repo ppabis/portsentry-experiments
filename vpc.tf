70b149e96b2d693b8671a623ee54007a668af3e0
ppabis <piotr@pabis.eu>
2025-06-22 19:19:31 +0200

    Formatting

--- vpc.tf
+++ vpc.tf
@@ -5,18 +5,12 @@ module "vpc" {
   name = "portsentry-vpc"
   cidr = "10.199.199.0/24"
 
-  azs            = ["eu-west-2a", "eu-west-2b"]
-  public_subnets = ["10.199.199.0/26", "10.199.199.64/26"]
-
-  # Disable NAT gateways since we only want public subnets
-  enable_nat_gateway = false
-  single_nat_gateway = false
-
-  # Enable DNS hostnames and support
+  azs                  = ["eu-west-2a", "eu-west-2b"]
+  public_subnets       = ["10.199.199.0/26", "10.199.199.64/26"]
+  enable_nat_gateway   = false
+  single_nat_gateway   = false
   enable_dns_hostnames = true
   enable_dns_support   = true
-
-  # Tags
   public_subnet_tags = {
     Type = "Public"
   }
