eaff563c870c0c38e3416dbdf03222824579b244
ppabis <piotr@pabis.eu>
2025-06-22 19:09:17 +0200

    Init provider

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  
+  default_tags {
+    tags = {
+      Project     = "portsentry-experiments"
+    }
+  }
+} 
