9932c20c6975c40143064a032ec1a76e35e4d216
ppabis <piotr@pabis.eu>
2025-06-23 12:43:01 +0200

    Fix portsentry and docker installation

--- blue_team.sh
+++ blue_team.sh
@@ -3,16 +3,15 @@
 hostnamectl set-hostname blue-team
 
 # Install portsentry
+export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install portsentry -y
 
 # Install Docker
 apt install ca-certificates curl gnupg lsb-release -y
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" \
+ | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl enable --now docker
